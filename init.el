118a73b264314d50da1aab3e1dc24ab5dce3e209
Jason Foutz <jfoutz@gmail.com>
2015-01-06 22:33:31 -0700

    helm

--- init.el
+++ init.el
@@ -1,1 +1,31 @@
+;;; cribbed from https://github.com/jordonbiondo/.emacs.d/blob/master/init.el
 
+
+;; initial setup
+(setq visible-bell 1)
+
+(tool-bar-mode -1)
+
+;; package manager configuration
+(mapc (lambda (p) (push p load-path))
+      '("~/.emacs.d/use-package/"))
+
+(require 'use-package)
+(require 'package)
+
+(mapc (lambda(p) (push p package-archives))
+      '(("marmalade" . "http://marmalade-repo.org/packages/")
+        ("melpa" . "http://melpa.milkbox.net/packages/")))
+(package-refresh-contents)
+(package-initialize)
+
+;; packages
+
+(use-package magit
+  :ensure t)
+
+(use-package helm
+  :ensure t
+  :init
+  (progn
+    (helm-mode))
